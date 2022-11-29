ad6ad913ee1a6df1b9f99fbc9a986748ce61432e
Nisida Masaya <mamamanoma297@icloud.com>
2022-11-29 09:52:41 +0900

    Create Project

---
+++ src/resources/ts/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import { ChakraProvider, Button } from "@chakra-ui/react";
+// import { BrowserRouter } from "react-router-dom";
+
+// import theme from "./theme/theme";
+// import { Router } from "./router/Router";
+
+export default function App() {
+    return (
+        <>
+        <p>テスト</p>
+        </>
+        // <ChakraProvider theme={theme}>
+        //     <BrowserRouter>
+        //         <Router />
+        //     </BrowserRouter>
+        // </ChakraProvider>
+    );
+}
