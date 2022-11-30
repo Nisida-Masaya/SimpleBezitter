a6b9458e96f10be765cf944bfd2789b8a9e86559
Nisida Masaya <mamamanoma297@icloud.com>
2022-11-30 10:07:05 +0900

    Update :

--- src/resources/ts/component/molecules/MenuDrawer.tsx
+++ src/resources/ts/component/molecules/MenuDrawer.tsx
@@ -59,7 +59,19 @@ export const MenuDrawer: VFC<Props> = memo((props) => {
                             </svg>
                             プロフィール
                         </Button>
-                        <Button w="100%">パスワード変更</Button>
+                        <Button w="100%">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                fill="currentColor"
+                                className="bi bi-key-fill"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                            </svg>
+                            パスワード変更
+                        </Button>
                     </DrawerBody>
                 </DrawerContent>
             </DrawerOverlay>
